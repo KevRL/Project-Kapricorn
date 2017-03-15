b0a4585fcf2797ecb193dfd5d33efd56148be345
royfagon <roy.fagon@kantarmedia.com>
2017-03-15 15:04:08 +0000

    Added registration

--- api/Kapricorn/App_Start/WebApiConfig.cs
+++ api/Kapricorn/App_Start/WebApiConfig.cs
@@ -2,39 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Kapricorn
 {
-    public static class WebApiConfig
+    public class WebApiConfig
     {
+        public MongoClient mClient = null;
+        public MongoServer globalserver = null;
+        public MongoDatabase dbMongoREAD = null;
+        public string s_Database;
+        public string s_Collection;
+
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            /*
             config.Routes.MapHttpRoute(
                 name: "RegisterApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "api/register/{id}",
+                defaults: new { controller = "Register", id = RouteParameter.Optional }
+            );
+            */
+            config.Routes.MapHttpRoute(
+                name: "RegisterApi2",
+                routeTemplate: "api/register/{name_id}/{email_id}/{pswd_id}",
+                defaults: new { controller = "Register", name_id = RouteParameter.Optional, email_id = RouteParameter.Optional, pswd_id = RouteParameter.Optional }
             );
 
             config.Routes.MapHttpRoute(
                 name: "DataApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/data/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
 
             config.Routes.MapHttpRoute(
                 name: "SurveysApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/surveys/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
 
             config.Routes.MapHttpRoute(
                 name: "UsersApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/users/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
--- api/Kapricorn/Controllers/RegisterController.cs
+++ api/Kapricorn/Controllers/RegisterController.cs
@@ -4,10 +4,28 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MongoDB.Bson;
+using MongoDB.Driver;
+//using Newtonsoft.Json;
 
 namespace Kapricorn.Controllers
 {
-    public class RegisterController : ApiController
+        public class RegisterController : ApiController
     {
+        public HttpResponseMessage GetData()
+        {
+            string json = "[{\"GetData\":\"RWH\"}]";
+            var respx = new HttpResponseMessage { Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json") };
+            return respx;
+        }
+
+       
+        public HttpResponseMessage GetLogin(string name_id, string email_id, string pswd_id)
+        {
+            string json = "[{\"name\":\"" + name_id + "\"}, {\"email\":\"" + email_id + "\"}, {\"pswd\":\"" + pswd_id + "\"}]";
+            var respx = new HttpResponseMessage { Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json") };
+            return respx;
+        }
+
     }
 }
